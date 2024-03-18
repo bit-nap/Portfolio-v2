7e9894d3d1117017eacfbff2884e784ca4d2c0dc
Norton Andres Perez <nap1118@g.rit.edu>
2024-03-17 21:45:51 -0400

    deployment update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -8,17 +8,17 @@ import "./App.css";
 
 function App() {
   return (
-    <HashRouter basename="/Portfolio">
+    <BrowserRouter basename="https://bit-nap.github.io/Portfolio/">
       <div className="App">
         <NavBar></NavBar>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
+          <Route path="" element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
         </Routes>
         <Footer></Footer>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
